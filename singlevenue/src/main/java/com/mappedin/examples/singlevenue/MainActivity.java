29e0aeff463b70971d6ae0e447f9a9f36c8699b0
Paul Bernhardt <paul@mappedin.ca>
2016-06-09 15:43:53 -0400

    Roughed in Single Venue example

---
+++ singlevenue/src/main/java/com/mappedin/examples/singlevenue/MainActivity.java
@@ -0,0 +1,90 @@
+package com.mappedin.examples.singlevenue;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+import com.mappedin.sdk.Location;
+import com.mappedin.sdk.LocationGenerator;
+import com.mappedin.sdk.MapView;
+import com.mappedin.sdk.Map;
+import com.mappedin.sdk.MappedinCallback;
+import com.mappedin.sdk.MappedIn;
+import com.mappedin.sdk.RawData;
+import com.mappedin.sdk.Venue;
+import com.mappedin.jpct.Logger;
+
+public class MainActivity extends AppCompatActivity {
+
+    private boolean accessibleDirections = false;
+
+    private MappedIn mappedIn = null;
+    private MapView mapView = null;
+    private Map[] maps = null;
+
+    private Venue activeVenue = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mappedIn = new MappedIn(getApplicationContext());
+        mappedIn.getVenues(new GetVenuesCallback());
+    }
+
+    // Get the basic info for all Venues we have access to
+    private class GetVenuesCallback implements MappedinCallback<List<Venue>> {
+        @Override
+        public void onCompleted(final List<Venue> venues) {
+            if (venues.size() == 0 ) {
+                Logger.log("No venues available! Are you using the right credentials? Talk to your mappedin representative.");
+                return;
+            }
+            activeVenue = venues.get(0); // Grab the first venue, which is likely all you have
+            setTitle(activeVenue.getName());
+            mapView = (MapView) getFragmentManager().findFragmentById(R.id.mapFragment);
+            mappedIn.getVenue(activeVenue, accessibleDirections, new CustomLocationGenerator(), new GetVenueCallback());
+        }
+
+        @Override
+        public void onError(Exception e) {
+            Logger.log("Error loading any venues. Did you set your credentials? Exception: " + e);
+        }
+    }
+
+    // Get the full details on a single Venue
+    private class GetVenueCallback implements MappedinCallback<Venue> {
+        @Override
+        public void onCompleted(final Venue venue) {
+            venue.getMaps().toArray(maps);
+            if (maps.length == 0) {
+                Logger.log("No maps! Make sure your venue is set up correctly!");
+                return;
+            }
+            Arrays.sort(maps, new Comparator<Map>() {
+                @Override
+                public int compare(Map a, Map b) {
+                    return (int) (a.getElevation() - b.getElevation());
+                }
+            });
+            mapView.setMap(maps[0]);
+        }
+
+        @Override
+        public void onError(Exception e) {
+            Logger.log("Error loading Venue: " + e);
+        }
+
+    }
+
+    private class CustomLocationGenerator implements LocationGenerator {
+        @Override
+        public Location locationGenerator(RawData rawData) throws Exception {
+            return new Location(rawData);
+        }
+    }
+}
