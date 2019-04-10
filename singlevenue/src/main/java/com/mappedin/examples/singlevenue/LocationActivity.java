879532598b5b699b076bd20fc9fef3b8796dcdf8
Christine Maiolo <maiolo.c313@gmail.com>
2019-04-10 13:28:52 -0400

    Removing unused import

--- singlevenue/src/main/java/com/mappedin/examples/singlevenue/LocationActivity.java
+++ singlevenue/src/main/java/com/mappedin/examples/singlevenue/LocationActivity.java
@@ -8,7 +8,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.koushikdutta.ion.Ion;
