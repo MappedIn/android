a2ad9016e0a0e6d8604558ec8b066c43a182b088
Saman Sandhu <saman_sandhu@outlook.com>
2021-08-18 17:03:38 -0400

    SDK version bump

--- SDKv3_Examples/app/src/main/java/com/mappedin/sdkv3_examples/MainActivity.kt
+++ SDKv3_Examples/app/src/main/java/com/mappedin/sdkv3_examples/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.mappedin.sdkv3_examples
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.mappedin.sdk.listeners.MPIMapViewListener
 import com.mappedin.sdk.models.*
@@ -14,6 +14,7 @@ import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import java.util.*
+import kotlin.concurrent.schedule
 import kotlin.concurrent.timerTask
 import kotlin.math.sqrt
 
@@ -29,7 +30,7 @@ class MainActivity : AppCompatActivity() {
     private val connectionTemplateString: String by lazy { readFileContentFromAssets("connectionTemplate.html") }
     private val venueDataString: String by lazy { readFileContentFromAssets("mappedin-demo-mall.json") }
     private val markerString: String by lazy { readFileContentFromAssets("marker.html") }
-    private val positionsString: String by lazy { readFileContentFromAssets("position.json").replace("\n", "") }
+    private val positionsString: String by lazy { readFileContentFromAssets("positions.json").replace("\n", "") }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -114,7 +115,7 @@ class MainActivity : AppCompatActivity() {
         mapView.listener = object : MPIMapViewListener {
             override fun onDataLoaded(data: MPIData) {
                 println("MPIData: " + Json.encodeToString(data))
-                sortedMaps = data.maps.sortedBy{it.elevation}
+                sortedMaps = data.maps.sortedBy { it.elevation }
 
                 // Enable blueDot, does not appear until updatePosition is called with proper coordinates
                 mapView.blueDotManager.enable(MPIOptions.BlueDot(smoothing = false, showBearing = true, baseColor = "#2266ff"))
@@ -125,6 +126,7 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
             }
+
             override fun onMapChanged(map: MPIMap) {
                 runOnUiThread {
                     supportActionBar?.title = map.name
@@ -132,18 +134,17 @@ class MainActivity : AppCompatActivity() {
                 println("MPIMap Changed: " + Json.encodeToString(map))
 
                 // Create an MPICoordinate from Latitude and Longitude
-                val coord = map.createCoordinate(43.5214,-80.5369)
+                val coord = map.createCoordinate(43.5214, -80.5369)
                 println("MPICoordinate: $coord")
 
                 // Find Distance between Location and Nearest Node
                 val distance = distanceLocationToNode(map, 43.5214, -80.5369)
                 println("Distance: $distance")
             }
+
             override fun onPolygonClicked(polygon: MPINavigatable.MPIPolygon) {
                 println("MPIPolygon Clicked:" + Json.encodeToString(polygon))
-                runOnUiThread {
-                    selectPolygon(polygon)
-                }
+                selectPolygon(polygon)
             }
 
             override fun onStateChanged(state: MPIState) {
@@ -163,7 +164,9 @@ class MainActivity : AppCompatActivity() {
 
             override fun onBlueDotPositionUpdate(update: MPIBlueDotPositionUpdate) {
                 this@MainActivity.blueDot = update
-                nearestNode.text = getString(R.string.blueDotNearestNode, update.nearestNode?.id ?: "N/A")
+                runOnUiThread {
+                    nearestNode.text = getString(R.string.blueDotNearestNode, update.nearestNode?.id ?: "N/A")
+                }
             }
 
             override fun onBlueDotStateChange(stateChange: MPIBlueDotStateChange) {
@@ -171,68 +174,65 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onNothingClicked() {
-                runOnUiThread {
-                    clearPolygon()
-                }
+                clearPolygon()
             }
 
             override fun onFirstMapLoaded() {
-                runOnUiThread {
-                    defaultRotation = defaultRotation ?: mapView.cameraControlsManager.rotation
-                    defaultTilt = defaultTilt ?: mapView.cameraControlsManager.tilt
+                defaultRotation = defaultRotation ?: mapView.cameraControlsManager.rotation
+                defaultTilt = defaultTilt ?: mapView.cameraControlsManager.tilt
 
-                    mapView.cameraControlsManager.setRotation(180.0)
-                    mapView.cameraControlsManager.setTilt(0.0)
+                mapView.cameraControlsManager.setRotation(180.0)
+                mapView.cameraControlsManager.setTilt(0.0)
 
-                    // label all locations to be light on dark
-                    mapView.labelAllLocations(
-                        options=MPIOptions.LabelAllLocations(
-                            appearance=MPIOptions.LabelAppearance.lightOnDark
-                        )
+                // label all locations to be light on dark
+                mapView.labelAllLocations(
+                    options = MPIOptions.LabelAllLocations(
+                        appearance = MPIOptions.LabelAppearance.lightOnDark
                     )
+                )
 
-                    // create a multi-destination journey between 4 sample locations
-                    mapView.venueData?.locations?.let { locations ->
-                        if (locations.size < 8) return@let
+                // create a multi-destination journey between 4 sample locations
+                mapView.venueData?.locations?.let { locations ->
+                    if (locations.size < 8) return@let
 
-                        mapView.getDirections(
-                            to=MPIDestinationSet(destinations= listOf(locations[4], locations[5], locations[6])),
-                            from=locations[7],
-                            accessible=false
-                        ) {
-                            it?.let { directions ->
-                                // draw the journey
-                                mapView.journeyManager.draw(
-                                    directions=directions,
-                                    options=MPIOptions.Journey(connectionTemplateString=connectionTemplateString)
-                                )
+                    mapView.getDirections(
+                        to = MPIDestinationSet(destinations = listOf(locations[4], locations[5], locations[6])),
+                        from = locations[7],
+                        accessible = false
+                    ) {
+                        it?.let { directions ->
+                            // draw the journey
+                            mapView.journeyManager.draw(
+                                directions = directions,
+                                options = MPIOptions.Journey(connectionTemplateString = connectionTemplateString)
+                            )
 
-                                val maxSteps = 3
-                                val startDelay = 15
-                                val stepDelay = 5
+                            val maxSteps = 3
+                            val startDelay = 15
+                            val stepDelay = 5
+                            val timer = Timer("Step Setter", false)
 
-                                for (step in 0..maxSteps) {
-                                    // manipulate journey after a delay
-                                    Timer(false).schedule(timerTask {
-                                        if (step == maxSteps) {
-                                            // change the journey step
-                                            mapView.journeyManager.clear()
-                                        } else {
-                                            // clear journey
-                                            mapView.journeyManager.setStep(step)
-                                        }
-                                    }, (startDelay + stepDelay * step) * 1000L)
+                            for (step in 0..maxSteps) {
+                                // manipulate journey after a delay
+                                timer.schedule((startDelay + stepDelay * step) * 1000L) {
+                                    if (step == maxSteps) {
+                                        // change the journey step
+                                        mapView.journeyManager.clear()
+                                    } else {
+                                        // clear journey
+                                        mapView.journeyManager.setStep(step)
+                                    }
                                 }
                             }
                         }
                     }
+                }
 
-                    val positions = Json.decodeFromString<List<MPIPosition>>(positionsString)
-                    val handler = Handler()
-                    positions.forEachIndexed { index, position ->
-                        handler.postDelayed({
-                            mapView.blueDotManager.updatePosition(position)
-                        }, 3000L * index)
+                val positions = Json.decodeFromString<List<MPIPosition>>(positionsString)
+                val timer = Timer("Position Updater", false)
+                positions.forEachIndexed { index, position ->
+                    timer.schedule(3000L * index) {
+                        mapView.blueDotManager.updatePosition(position)
                     }
                 }
             }
@@ -260,8 +260,10 @@ class MainActivity : AppCompatActivity() {
 
     fun clearPolygon() {
         selectedPolygon = null
-        locationView.visibility = View.GONE
         mapView.clearAllPolygonColors()
+        runOnUiThread {
+            locationView.visibility = View.GONE
+        }
     }
 
     fun distanceLocationToNode(map: MPIMap, latitude: Double, longitude: Double): Double? {
@@ -285,14 +287,12 @@ class MainActivity : AppCompatActivity() {
 
     fun selectPolygon(polygon: MPINavigatable.MPIPolygon) {
         polygon.locations.firstOrNull()?.let {
-
             selectedPolygon = polygon
-            locationView.visibility = View.VISIBLE
 
             mapView.clearAllPolygonColors {
                 mapView.setPolygonColor(polygon.id, "blue")
             }
-            mapView.focusOn(MPIOptions.Focus(polygons=listOf(polygon)))
+            mapView.focusOn(MPIOptions.Focus(polygons = listOf(polygon)))
 
             if (markerId == presentMarkerId) {
                 mapView?.removeMarker(markerId)
@@ -303,20 +303,23 @@ class MainActivity : AppCompatActivity() {
             markerId = mapView.createMarker(node, markerString, MPIOptions.Marker(anchor = MPIOptions.MARKER_ANCHOR.TOP))
             presentMarkerId = markerId
 
-            locationTitle.text = it.name
-            locationDescription.text = it.description
-            Glide
-                .with(this@MainActivity)
-                .load(it.logo?.original)
-                .centerCrop()
-                .into(locationImage)
+            runOnUiThread {
+                locationView.visibility = View.VISIBLE
+                locationTitle.text = it.name
+                locationDescription.text = it.description
+                Glide
+                    .with(this@MainActivity)
+                    .load(it.logo?.original)
+                    .centerCrop()
+                    .into(locationImage)
+            }
         }
     }
 
     private fun changeMap(isIncrementing: Boolean = true) {
         mapView.currentMap?.let {
             val currentIndex = sortedMaps?.indexOf(it) ?: 0
-            val nextIndex = if(isIncrementing) currentIndex+1 else currentIndex-1
+            val nextIndex = if (isIncrementing) currentIndex + 1 else currentIndex - 1
             sortedMaps?.getOrNull(nextIndex)?.let { nextMap ->
                 mapView.setMap(nextMap)
             }
