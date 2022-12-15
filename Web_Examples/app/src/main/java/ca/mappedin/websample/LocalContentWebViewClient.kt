b7eea0b6283b956e437c7e20e12c9502318a1d30
Jere Suikkila <jere.suikkila@mappedin.ca>
2022-12-15 17:36:41 -0500

    feat: update to mappedin web v2 example

---
+++ Web_Examples/app/src/main/java/ca/mappedin/websample/LocalContentWebViewClient.kt
@@ -0,0 +1,27 @@
+package ca.mappedin.websample
+
+import android.net.Uri
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
+import android.webkit.WebView
+import androidx.annotation.RequiresApi
+import androidx.webkit.WebViewAssetLoader
+import androidx.webkit.WebViewClientCompat
+
+class LocalContentWebViewClient(private val assetLoader: WebViewAssetLoader) : WebViewClientCompat() {
+    @RequiresApi(21)
+    override fun shouldInterceptRequest(
+        view: WebView,
+        request: WebResourceRequest
+    ): WebResourceResponse? {
+        return assetLoader.shouldInterceptRequest(request.url)
+    }
+
+    // to support API < 21
+    override fun shouldInterceptRequest(
+        view: WebView,
+        url: String
+    ): WebResourceResponse? {
+        return assetLoader.shouldInterceptRequest(Uri.parse(url))
+    }
+}
