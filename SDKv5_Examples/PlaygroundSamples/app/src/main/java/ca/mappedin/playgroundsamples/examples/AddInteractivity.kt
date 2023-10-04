ccb553274e02aa696e06defbb48bc2cdc529f7f1
Mark Sohm <mark.sohm@mappedin.ca>
2023-10-04 14:58:56 -0400

    Added new interactive objects to sample

--- SDKv5_Examples/PlaygroundSamples/app/src/main/java/ca/mappedin/playgroundsamples/examples/AddInteractivity.kt
+++ SDKv5_Examples/PlaygroundSamples/app/src/main/java/ca/mappedin/playgroundsamples/examples/AddInteractivity.kt
@@ -2,6 +2,7 @@ package ca.mappedin.playgroundsamples.examples
 
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import ca.mappedin.playgroundsamples.R
 import com.mappedin.sdk.MPIMapView
@@ -10,7 +11,7 @@ import com.mappedin.sdk.listeners.MPIMapViewListener
 import com.mappedin.sdk.models.*
 import com.mappedin.sdk.web.MPIOptions
 
-class AddInteractivity : AppCompatActivity(), MPIMapClickListener {
+class AddInteractivity : AppCompatActivity(), MPIMapViewListener, MPIMapClickListener {
     private lateinit var mapView: MPIMapView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,12 +30,104 @@ class AddInteractivity : AppCompatActivity(), MPIMapClickListener {
             ),
         ) { Log.e(javaClass.simpleName, "Error loading map view") }
         mapView.mapClickListener = this
+        mapView.listener = this
     }
 
     override fun onClick(mapClickEvent: MPIMapClickEvent) {
-        if (!mapClickEvent.polygons.isEmpty()) {
-            val polygon = mapClickEvent.polygons.first()
-            mapView.setPolygonColor(polygon, "#BF4320")
+        var title = String()
+        val message = StringBuilder()
+
+        // Use the map name as the title.
+        if (!mapClickEvent.maps.isEmpty()) {
+            title = mapClickEvent.maps.first().name
         }
+
+        // If a floating label was clicked, add its text to the message.
+        if (!mapClickEvent.floatingLabels.isEmpty()) {
+            message.append("Floating Label Clicked: ")
+            message.append(mapClickEvent.floatingLabels.first().second)
+            message.append("\n")
+        }
+
+        // If a polygon was clicked, add it's location name to the message.
+        if (!mapClickEvent.polygons.isEmpty()) {
+            message.append("Polygon clicked: ")
+            message.append(mapClickEvent.polygons.first().locations.first().name)
+            message.append("\n")
+        }
+
+        // If a path was clicked, add it to the message.
+        if (!mapClickEvent.paths.isEmpty()) {
+            message.append("You clicked a path.\n")
+        }
+
+        // Add the coordinates clicked to the message.
+        message.append("Coordinate Clicked: \nLatitude: ")
+        message.append(mapClickEvent.position?.latitude)
+        message.append("\nLongitude: ")
+        message.append(mapClickEvent.position?.longitude)
+
+        showMessage(title, message.toString())
+    }
+
+    override fun onBlueDotPositionUpdate(update: MPIBlueDotPositionUpdate) {}
+
+    override fun onBlueDotStateChange(stateChange: MPIBlueDotStateChange) {}
+
+    override fun onDataLoaded(data: MPIData) {}
+
+    override fun onFirstMapLoaded() {
+        // Make the floating labels interactive.
+        mapView.floatingLabelsManager.labelAllLocations(MPIOptions.FloatingLabelAllLocations(interactive = true))
+
+        // Draw an interactive journey.
+        var departure = mapView.venueData?.locations?.first { it.name == "Microsoft" }
+        var destination = mapView.venueData?.locations?.first { it.name == "Apple" }
+
+        if (departure == null || destination == null) return
+
+        val journeyOpts = MPIOptions.Journey(pathOptions = MPIOptions.Path(interactive = true))
+
+        mapView.getDirections(to = destination, from = departure) {
+            if (it != null) {
+                mapView.journeyManager.draw(directions = it, options = journeyOpts)
+            }
+        }
+
+        // Draw an interactive path.
+        departure = mapView.venueData?.locations?.first { it.name == "Uniqlo" }
+        destination = mapView.venueData?.locations?.first { it.name == "Nespresso" }
+
+        if (departure == null || destination == null) return
+
+        mapView.getDirections(to = destination, from = departure) {
+            if (it != null) {
+                mapView.pathManager.add(it.path, MPIOptions.Path(interactive = true))
+            }
+        }
+    }
+
+    override fun onMapChanged(map: MPIMap) {}
+
+    @Deprecated("Use MPIMapClickListener instead")
+    override fun onNothingClicked() {}
+
+    @Deprecated("Use MPIMapClickListener instead")
+    override fun onPolygonClicked(polygon: MPINavigatable.MPIPolygon) {}
+
+    override fun onStateChanged(state: MPIState) {}
+
+    private fun showMessage(title: String, message: String) {
+        runOnUiThread(
+            Runnable {
+                val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+                builder
+                    .setMessage(message)
+                    .setTitle(title)
+
+                val dialog: AlertDialog = builder.create()
+                dialog.show()
+            },
+        )
     }
 }
