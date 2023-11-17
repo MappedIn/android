ea23aa11cf9b000eaf026deab6365aba28bee1e7
Mark Sohm <mark.sohm@mappedin.ca>
2023-11-17 14:31:19 -0500

    Add Tooltips

--- SDKv5_Examples/PlaygroundSamples/app/src/main/java/ca/mappedin/playgroundsamples/data/Datasource.kt
+++ SDKv5_Examples/PlaygroundSamples/app/src/main/java/ca/mappedin/playgroundsamples/data/Datasource.kt
@@ -11,6 +11,7 @@ import ca.mappedin.playgroundsamples.examples.ListLocations
 import ca.mappedin.playgroundsamples.examples.Markers
 import ca.mappedin.playgroundsamples.examples.RenderMap
 import ca.mappedin.playgroundsamples.examples.Search
+import ca.mappedin.playgroundsamples.examples.Tooltips
 import ca.mappedin.playgroundsamples.examples.TurnByTurnDirections
 import ca.mappedin.playgroundsamples.model.Example
 
@@ -26,6 +27,7 @@ class Datasource {
             Example("Blue Dot", "Display the Blue Dot on the map", BlueDot::class.java),
             Example("Camera Controls", "Set, animate or focus the camera on a set of map objects", CameraControls::class.java),
             Example("List Locations", "List locations of a venue without rendering the map", ListLocations::class.java),
+            Example("Tooltips", "Adding HTML tooltips to the map view", Tooltips::class.java),
             Example("Building & Level Selection", "Add a building & level selector", LevelSelector::class.java),
             Example("Turn-by-Turn Directions", "Display text-based turn-by-turn directions", TurnByTurnDirections::class.java),
             Example("Search", "Search locations within a venue", Search::class.java),
---
+++ SDKv5_Examples/PlaygroundSamples/app/src/main/java/ca/mappedin/playgroundsamples/examples/Tooltips.kt
@@ -0,0 +1,90 @@
+package ca.mappedin.playgroundsamples.examples
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import ca.mappedin.playgroundsamples.R
+import com.mappedin.sdk.MPIMapView
+import com.mappedin.sdk.listeners.MPIMapClickListener
+import com.mappedin.sdk.listeners.MPIMapViewListener
+import com.mappedin.sdk.models.MPIBlueDotPositionUpdate
+import com.mappedin.sdk.models.MPIBlueDotStateChange
+import com.mappedin.sdk.models.MPIData
+import com.mappedin.sdk.models.MPIMap
+import com.mappedin.sdk.models.MPIMapClickEvent
+import com.mappedin.sdk.models.MPINavigatable
+import com.mappedin.sdk.models.MPIState
+import com.mappedin.sdk.web.MPIOptions
+
+class Tooltips : AppCompatActivity(), MPIMapViewListener, MPIMapClickListener {
+    private lateinit var mapView: MPIMapView
+    private val toolTipIds = mutableListOf<String>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_example)
+        this.title = "Tooltips"
+
+        mapView = findViewById<MPIMapView>(R.id.mapView)
+        // See Trial API key Terms and Conditions
+        // https://developer.mappedin.com/api-keys/
+        mapView.loadVenue(
+            MPIOptions.Init(
+                "5eab30aa91b055001a68e996",
+                "RJyRXKcryCMy4erZqqCbuB1NbR66QTGNXVE0x3Pg6oCIlUR1",
+                "mappedin-demo-mall",
+            ),
+            showVenueOptions = MPIOptions.ShowVenue(labelAllLocationsOnInit = false),
+        ) { Log.e(javaClass.simpleName, "Error loading map view") }
+        mapView.listener = this
+        mapView.mapClickListener = this
+    }
+
+    override fun onBlueDotPositionUpdate(update: MPIBlueDotPositionUpdate) {
+    }
+
+    override fun onBlueDotStateChange(stateChange: MPIBlueDotStateChange) {
+    }
+
+    override fun onDataLoaded(data: MPIData) {
+    }
+
+    override fun onFirstMapLoaded() {
+        mapView.flatLabelsManager.labelAllLocations(MPIOptions.FlatLabelAllLocations())
+    }
+
+    override fun onMapChanged(map: MPIMap) {
+    }
+
+    @Deprecated("Use MPIMapClickListener instead.")
+    override fun onNothingClicked() {
+    }
+
+    override fun onClick(mapClickEvent: MPIMapClickEvent) {
+        if (!mapClickEvent.polygons.isEmpty()) {
+            mapView.createTooltip(
+                node = mapClickEvent.polygons.first().entrances[0],
+                """<div tabindex="0" style="padding: 10px; background-color: gray;">${mapClickEvent.polygons.first().locations[0].name}</div>""",
+                MPIOptions.Tooltip(
+                    collisionRank = MPIOptions.COLLISION_RANK.MEDIUM,
+                )){
+                    id ->
+                if (id != null) {
+                    toolTipIds.add(id)
+                }
+            }
+        } else {
+            toolTipIds.forEach {
+                mapView.removeTooltip(it)
+            }
+            toolTipIds.clear()
+        }
+    }
+
+    @Deprecated("Use MPIMapClickListener instead.")
+    override fun onPolygonClicked(polygon: MPINavigatable.MPIPolygon) {
+    }
+
+    override fun onStateChanged(state: MPIState) {
+    }
+}
